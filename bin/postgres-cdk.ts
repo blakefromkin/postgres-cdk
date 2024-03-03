05091ddb9d233d62418af5bf2604e8d988e5e770
Blake Fromkin <blakefromkin@gmail.com>
2024-03-03 15:00:35 -0500

    Initial commit

---
+++ bin/postgres-cdk.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { PostgresCdkStack } from '../lib/postgres-cdk-stack';
+
+const app = new cdk.App();
+new PostgresCdkStack(app, 'PostgresCdkStack');
